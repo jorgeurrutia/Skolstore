e0454c5137cc0762bddd9b449be0abd10edf69af
Jorge Urrutia <jorge.urrutia@live.cl>
2022-10-25 14:19:55 -0300

    Agregar cambios

--- src/index.js
+++ src/index.js
@@ -1,23 +1,47 @@
 import express from "express";
 import { dirname, join} from "path"
 import { fileURLToPath} from "url"
+import  logger  from "morgan"
 
 import indexRoutes from "./routes/index.js"
 
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url))
-console.log(join(__dirname,"views"))
 
 //settings
 app.set("views", join(__dirname, "views"))
 app.set("view engine", "ejs")
+app.set('port', process.env.PORT || 3000);
 
 //routes
 app.use(indexRoutes)
 
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 //statis files
 app.use(express.static(join(__dirname, "public")))
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+  });
+
+  // error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+
+  //export default app;
+
 //listening
-app.listen(3000);
-console.log("Server on port", 3000);
+app.listen(app.get('port'), () => {
+    console.log("Server on port", app.get('port'));
+});
